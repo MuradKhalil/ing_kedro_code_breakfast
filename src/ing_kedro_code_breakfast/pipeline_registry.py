77c6f602d936e0d77c425c122e700af151454a89
MuradKhalil <murad.khalilov@pm.me>
2022-01-18 22:30:09 +0100

    added complete code with solutions

--- src/ing_kedro_code_breakfast/pipeline_registry.py
+++ src/ing_kedro_code_breakfast/pipeline_registry.py
@@ -30,6 +30,8 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline
+from ing_kedro_code_breakfast.pipelines import data_processing as dp
+from ing_kedro_code_breakfast.pipelines import data_science as ds
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -38,4 +40,11 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": Pipeline([])}
+    data_processing_pipeline = dp.create_pipeline()
+    data_science_pipeline = ds.create_pipeline()
+
+    return {
+        "__default__": data_processing_pipeline + data_science_pipeline,
+        "dp": data_processing_pipeline,
+        "ds": data_science_pipeline,
+    }
---
+++ src/ing_kedro_code_breakfast/pipelines/data_processing/pipeline.py
@@ -0,0 +1,26 @@
+from kedro.pipeline import Pipeline, node
+from .nodes import preprocess_companies, preprocess_shuttles, create_model_input_table
+
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            node(
+                func=preprocess_companies,
+                inputs="companies",
+                outputs="preprocessed_companies",
+                name="preprocess_companies_node",
+            ),
+            node(
+                func=preprocess_shuttles,
+                inputs="shuttles",
+                outputs="preprocessed_shuttles",
+                name="preprocess_shuttles_node",
+            ),
+            node(
+                func=create_model_input_table,
+                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
+                outputs="model_input_table",
+                name="create_model_input_table_node",
+            ),
+        ]
+    )
